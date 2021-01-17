bcf27e508c23642fcd8d07e6e2a0ddf3bfefe2f6
gjpiret <gjpiret@yahoo.com>
2021-01-17 15:26:52 +0100

    Add project files.

---
+++ Schiphol/Controllers/FlightsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Schiphol.Models;
+
+namespace Schiphol.Controllers
+{
+	public class FlightsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		private async Task<string> CallApi(string apiaction, int page = 0)
+		{
+			using (var client = new HttpClient())
+			{
+				var url = "https://api.schiphol.nl/public-flights/" + apiaction;
+				var app_id = "e227c7da";
+				var app_key = "0188c9130d89d05acf91775c4da9d7fa";
+				var resourceversion = "v4";
+
+				string qs_page = "page=" + page.ToString();
+				url += "?" + qs_page;
+
+				client.DefaultRequestHeaders.Clear();
+				client.DefaultRequestHeaders.Add("app_id", app_id);
+				client.DefaultRequestHeaders.Add("app_key", app_key);
+				client.DefaultRequestHeaders.Add("resourceversion", resourceversion);
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+				using (HttpResponseMessage response = await client.GetAsync(url))
+				{
+					if (response.IsSuccessStatusCode)
+					{
+						using (HttpContent content = response.Content)
+						{
+							string result = await content.ReadAsStringAsync();
+							return result;
+						}
+					}
+				}
+			}
+
+			return string.Empty;
+		}
+
+		public async Task<IActionResult> GetFlights(int page = 0)
+		{
+			string result = await CallApi("flights", page);
+			try
+			{
+				var obj_result = JsonConvert.DeserializeObject<FlightList>(result);
+				return PartialView("Flights", obj_result);
+			}
+			catch (Exception ex)
+			{
+				return PartialView();
+			}
+		}
+
+		public async Task<IActionResult> GetAircraftTypes(int page = 0)
+		{
+			string result = await CallApi("aircrafttypes", page);
+			var obj_result = JsonConvert.DeserializeObject<AircraftTypeList>(result);
+			return PartialView("AircraftTypes", obj_result);
+		}
+	}
+}
---
+++ Schiphol/Models/Flights.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schiphol.Models
+{
+
+	public class AircraftTypeList
+	{
+		public List<AircraftType> AircraftTypes { get; set; }
+	}
+
+	public class AircraftType
+	{
+		public string IATAMain { get; set; }
+		public string IATASub { get; set; }
+		public string LongDescription { get; set; }
+		public string ShortDescription { get; set; }
+	}
+
+	public class Airline
+	{
+		public string IATA { get; set; }
+		public string Icao { get; set; }
+		public string Nvls { get; set; }
+		public string PublicName { get; set; }
+	}
+
+	public class Destination
+	{
+		public string City { get; set; }
+		public string Country { get; set; }
+		public string IATA { get; set; }
+		public string PublicName_NL { get; set; }
+		public string PublicName_EN { get; set; }
+	}
+
+	public class FlightList
+	{
+		public List<Flight> Flights { get; set; }
+	}
+
+	public class Flight
+	{
+		public string LastUpdatedAt { get; set; }
+		public DateTime? dtLastUpdatedAt
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.LastUpdatedAt))
+					return null;
+				else
+					return DateTime.Parse(this.LastUpdatedAt);
+			}
+		}
+		public string ActualLandingTime { get; set; }
+		public DateTime? dtActualLandingTime
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ActualLandingTime))
+					return null;
+				else
+					return DateTime.Parse(this.ActualLandingTime);
+			}
+		}
+		public string ActualOffBlockTime { get; set; }
+		public DateTime? dtActualOffBlockTime
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ActualOffBlockTime))
+					return null;
+				else
+					return DateTime.Parse(this.ActualOffBlockTime);
+			}
+		}
+		public string AircraftRegistration { get; set; }
+		public AircraftType? AircraftType { get; set; }
+		public BaggageClaimType? BaggageClaim { get; set; }
+		public CheckinAllocationsType? CheckinAllocations { get; set; }
+		public CodesharesType? Codeshares { get; set; }
+		public string EstimatedLandingTime { get; set; }
+		public DateTime? dtEstimatedLandingTime
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.EstimatedLandingTime))
+					return null;
+				else
+					return DateTime.Parse(this.EstimatedLandingTime);
+			}
+		}
+		public string ExpectedTimeBoarding { get; set; }
+		public DateTime? dtExpectedTimeBoarding
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ExpectedTimeBoarding))
+					return null;
+				else
+					return DateTime.Parse(this.ExpectedTimeBoarding);
+			}
+		}
+		public string ExpectedTimeGateClosing { get; set; }
+		public DateTime? dtExpectedTimeGateClosing
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ExpectedTimeGateClosing))
+					return null;
+				else
+					return DateTime.Parse(this.ExpectedTimeGateClosing);
+			}
+		}
+		public string ExpectedTimeGateOpen { get; set; }
+		public DateTime? dtExpectedTimeGateOpen
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ExpectedTimeGateOpen))
+					return null;
+				else
+					return DateTime.Parse(this.ExpectedTimeGateOpen);
+			}
+		}
+		public string ExpectedTimeOnBelt { get; set; }
+		public DateTime? dtExpectedTimeOnBelt
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ExpectedTimeOnBelt))
+					return null;
+				else
+					return DateTime.Parse(this.ExpectedTimeOnBelt);
+			}
+		}
+		public string ExpectedSecurityFilter { get; set; }
+		public string FlightDirection { get; set; }
+		public string FlightName { get; set; }
+		public int? FlightNumber { get; set; }
+		public string Gate { get; set; }
+		public string Pier { get; set; }
+		public string Id { get; set; }
+		public string MainFlight { get; set; }
+		public string PrefixIATA { get; set; }
+		public string PrefixICAO { get; set; }
+		public int? AirlineCode { get; set; }
+		public string PublicEstimatedOffBlockTime { get; set; }
+		public DateTime? dtPublicEstimatedOffBlockTime
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.PublicEstimatedOffBlockTime))
+					return null;
+				else
+					return DateTime.Parse(this.PublicEstimatedOffBlockTime);
+			}
+		}
+		public PublicFlightStateType? PublicFlightState { get; set; }
+		public RouteType? Route { get; set; }
+		public string ScheduleDateTime { get; set; }
+		public DateTime? dtScheduleDateTime
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ScheduleDateTime))
+					return null;
+				else
+					return DateTime.Parse(this.ScheduleDateTime);
+			}
+		}
+		public string ScheduleDate { get; set; }
+		public DateTime? dtScheduleDate
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.ScheduleDate))
+					return null;
+				else
+					return DateTime.Parse(this.ScheduleDate);
+			}
+		}
+		public string ScheduleTime { get; set; }
+		public string ServiceType { get; set; }
+		public string Terminal { get; set; }
+		public TransferPositionsType? TransferPositions { get; set; }
+		public string SchemaVersion { get; set; }
+	}
+
+	public class BaggageClaimType
+	{
+		public List<string> Belts { get; set; }
+	}
+
+	public class CheckinAllocationsType
+	{
+		public List<CheckinAllocationType> CheckinAllocations { get; set; }
+		public RemarksType? Remarks { get; set; }
+	}
+
+	public class CheckinAllocationType
+	{
+		public string EndTime { get; set; }
+		public RowsType? Rows { get; set; }
+		public string StartTime { get; set; }
+	}
+
+	public class TransferPositionsType
+	{
+		public List<int> TransferPositions { get; set; }
+	}
+
+	public class RemarksType
+	{
+		public List<string> Remarks { get; set; }
+	}
+
+	public class RowsType
+	{
+		List<RowType> Rows { get; set; }
+	}
+
+	public class RowType
+	{
+		public string Position { get; set; }
+		public DeskType? Desks { get; set; }
+	}
+
+	public class DesksType
+	{
+		public List<DeskType> Desks { get; set; }
+	}
+
+	public class DeskType
+	{
+		public CheckinClassType? CheckinClass { get; set; }
+		public int? Position { get; set; }
+	}
+
+	public class CheckinClassType
+	{
+		public string Code { get; set; }
+		public string Description { get; set; }
+	}
+
+	public class RouteType
+	{
+		public List<string> Destinations { get; set; }
+		public string EU { get; set; }
+		public bool Visa { get; set; }
+	}
+
+	public class PublicFlightStateType
+	{
+		public List<string> FlightStates { get; set; }
+	}
+
+	public class CodesharesType
+	{
+		public List<string> Codeshares { get; set; }
+	}
+}
